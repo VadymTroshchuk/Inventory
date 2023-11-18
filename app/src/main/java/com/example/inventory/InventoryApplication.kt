7a3063797101a8c5f3c4dd57ee1c01f142420a48
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-11-18 21:32:53 +0200

    add database instance

--- app/src/main/java/com/example/inventory/InventoryApplication.kt
+++ app/src/main/java/com/example/inventory/InventoryApplication.kt
@@ -1,6 +1,11 @@
 
 package com.example.inventory
 
+import ItemRoomDatabase
 import android.app.Application
 
-class InventoryApplication : Application()
+
+class InventoryApplication : Application(){
+    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this) }
+}
+
